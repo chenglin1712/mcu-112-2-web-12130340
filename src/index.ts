ec65e790f747c377b56d9abf5e92cbb38d3d947e
Yucheng <chenglin1712@icloud.com>
2024-04-17 14:42:53 +0800

    feat(ts): 利用類別方法設定產品是否顯示

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,7 @@ const products = [
   new Product(2, "金魚都能懂的 CSS 必學屬性", "博碩文化", 500, true, new Date(2024, 3, 10)),
 ];
 
-setProductDisplay(products[1], false);
+products[1].setDisplay(false);
 
 console.log(products);
 console.table(products);
-
-function setProductDisplay(product: Product, isShow: boolean): void {
-  product.isShow = isShow;
-}
