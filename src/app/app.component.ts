712d538e61a6429fc065f844f0970b574def9c93
Yucheng <chenglin1712@icloud.com>
2024-05-29 17:03:06 +0800

    feat(product): 建立產品卡片清單元件

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,12 @@
 import { Component } from '@angular/core';
+import { ProductCardListComponent } from './product-card-list/product-card-list.component';
 import { ProductCardComponent } from './product-card/product-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ProductCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  imports: [ProductCardListComponent, ProductCardComponent],
 })
-export class AppComponent {
-  productName = '書籍 A';
-  authors = '作者甲、作者乙、作者丙';
-  company = '博碩文化';
-  isShow = true;
-  imgUrl = 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img';
-  createDate = new Date();
-  price = 10000;
-}
+export class AppComponent {}
