fc6b2ff712e26c853b7e80bceef0e86c910ee13f
Yucheng <chenglin1712@icloud.com>
2024-06-12 16:29:12 +0800

    feat(header): 新增標題元件實作

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HeaderComponent } from './header/header.component';
 import { Component } from '@angular/core';
 import { ProductCardListComponent } from './product-card-list/product-card-list.component';
 import { ProductCardComponent } from './product-card/product-card.component';
@@ -8,7 +9,7 @@ import { Product } from './model/product';
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [ProductCardListComponent, ProductCardComponent],
+  imports: [HeaderComponent, ProductCardListComponent, ProductCardComponent],
 })
 export class AppComponent {
   products = [
